9f7ba2d16c6438d127157fe592263286055b6ee8
jalgas93 <jalgas1993@list.ru>
2021-05-31 17:13:58 +0500

    adrg

--- app/src/main/java/com/behinesprutrol/envo/match/MainActivity.kt
+++ app/src/main/java/com/behinesprutrol/envo/match/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.behinesprutrol.envo.match
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +11,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findNavController(R.id.fragment_container)
+    }
+
+    private var backPressedTime:Long = 0
+    lateinit var backToast:Toast
+    override fun onBackPressed() {
+        backToast = Toast.makeText(this, "Press back again to leave the app.", Toast.LENGTH_LONG)
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            backToast.cancel()
+            super.onBackPressed()
+            return
+        } else {
+            backToast.show()
+        }
+        backPressedTime = System.currentTimeMillis()
     }
 }
